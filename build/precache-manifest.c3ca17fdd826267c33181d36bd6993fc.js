ad0a7eaf1dabc45dbbf292bf366242a72904ab9c
bharatchhabra <you@example.com>
2021-06-26 16:07:36 +0530

    project

---
+++ build/precache-manifest.c3ca17fdd826267c33181d36bd6993fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6370a8c85515f058934f",
+    "url": "/static/css/main.f7c9273d.chunk.css"
+  },
+  {
+    "revision": "6370a8c85515f058934f",
+    "url": "/static/js/main.6370a8c8.chunk.js"
+  },
+  {
+    "revision": "08d8e2eddd4b9861f06e",
+    "url": "/static/css/1.8c55a2d0.chunk.css"
+  },
+  {
+    "revision": "08d8e2eddd4b9861f06e",
+    "url": "/static/js/1.08d8e2ed.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c7f9f0a69a5900d2c8c8328834bd9654",
+    "url": "/index.html"
+  }
+];
